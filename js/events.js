2812bff231af0e6721d927a64bd054b12906899b
Dan Brimhall <dpbrimhall@hotmail.com>
2018-09-23 19:02:12 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -17,7 +17,7 @@ function pressIt() {
 }
 
 function submitIt() {
-  $('form:last-child').on('submit', function() {
+  $('form input:last-child').on('submit', function() {
     alert('Your form is going to be submitted now.')
   })
 }
